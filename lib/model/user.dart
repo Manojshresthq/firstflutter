891e1ef8a34ecfa150f8188f004e2eab00399817
Manoj Shrestha <shrestham684@gmail.com>
2022-11-30 10:26:44 +0545

    first commit

---
+++ lib/model/user.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_application_1/model/address.dart';
+import 'package:flutter_application_1/model/geo.dart';
+
+class User {
+  final String id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final String website;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+  });
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      phone: json['phone'],
+      website: json['website'],
+      address: Address.fromJson(json['id']),
+    );
+  }
+}
